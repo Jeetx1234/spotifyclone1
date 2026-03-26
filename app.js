029e55b30ad06920357deb9894ad0090df78eec3
Jeetx1234 <xgothambatman@gmail.com>
2026-03-26 22:54:18 +0530

    Refactor formatTime and getsongs functions

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function formatTime(seconds) {
 
 async function getsongs() {
 
-    let a = await fetch("./songs/")
+    let a = await fetch("songs/")
     let response = await a.text();
 
     let div = document.createElement("div")
